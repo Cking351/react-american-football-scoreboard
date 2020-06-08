79c80f033aa826025334beea5bc4b26453b798b1
Chris King <cking5836@gmail.com>
2020-06-08 15:28:10 -0400

    Met MVP

--- README.md
+++ README.md
@@ -52,17 +52,17 @@ const [value, setValue] = useState(); // Give these better names, and decide whe
 
 ### STEP 3 - Render the Scores to the DOM
 
-- [ ] The scores in the JSX are currently hardcoded to 32 points each. Remove the hardcoded values
-- [ ] Render the state values from what we just set up in steps 1 and 2
-- [ ] Play around with different initial values to test if they are rendering on the DOM correctly
+- [x] The scores in the JSX are currently hardcoded to 32 points each. Remove the hardcoded values
+- [x] Render the state values from what we just set up in steps 1 and 2
+- [x] Play around with different initial values to test if they are rendering on the DOM correctly
 
 ### STEP 4 - Add Click Functionality to Increment the Scores
 
-- [ ] Add the `onClick` handler to each function
-- [ ] Determine how much you will need to increment the score for each button
-  - [ ] A touchdown is worth 7 points (assume the following extra point is made)
-  - [ ] A field goal is worth 3 points
-- [ ] Inside the click handlers on each button, use the setter functions for each team to increment the appropriate team's score by the correct amount.
+- [x] Add the `onClick` handler to each function
+- [x] Determine how much you will need to increment the score for each button
+  - [x] A touchdown is worth 7 points (assume the following extra point is made)
+  - [x] A field goal is worth 3 points
+- [x] Inside the click handlers on each button, use the setter functions for each team to increment the appropriate team's score by the correct amount.
 
 ## Stretch Problems
 
--- src/App.js
+++ src/App.js
@@ -7,7 +7,23 @@ import "./App.css";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [ homeScore, setHomeScore ] = useState(0)
-  const [awayScore, setAwayScore ] = useState(0)
+  const [ awayScore, setAwayScore ] = useState(0)
+
+  const homeFieldGoalScore = event => {
+    setHomeScore(homeScore + 3)
+  }
+
+  const homeTouchdownScore = event => {
+    setHomeScore(homeScore + 7)
+  }
+
+  const awayFieldGoalScore = event => {
+    setAwayScore(awayScore + 3)
+  }
+
+  const awayTouchdownScore = event => {
+    setAwayScore(awayScore + 7)
+  }
 
   return (
     <div className="container">
@@ -31,12 +47,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={homeTouchdownScore}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={homeFieldGoalScore}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={awayTouchdownScore}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={awayFieldGoalScore}>Away Field Goal</button>
         </div>
       </section>
     </div>
